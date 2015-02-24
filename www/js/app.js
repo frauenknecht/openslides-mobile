f89c1f4656e0c728dee67965aa77d92532a3bf7e
Stefan Frauenknecht <stefan.frauenknecht@gmail.com>
2015-02-24 16:16:30 +0100

    Side menu added.

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,93 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('osl', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+	.run(function($ionicPlatform) {
+  		$ionicPlatform.ready(function() {
+    		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    		// for form inputs)
+    		if(window.cordova && window.cordova.plugins.Keyboard) {
+      			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    		}
+    		if(window.StatusBar) {
+      			StatusBar.styleDefault();
+    		}
+  		});
+	})
+	
+	.config(function ($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+
+			.state('app', {
+				url: '/app',
+				abstract: true,
+				templateUrl: 'templates/menu.html'
+    		})  
+			
+      		.state('app.projector', {
+				url: "/projector",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/projector.html"
+					}
+				}
+			})
+			
+			.state('app.agenda', {
+				url: "/agenda",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/agenda.html"
+					}
+				}
+			})  
+			
+			.state('app.motions', {
+				url: "/motions",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/motions.html"
+					}
+				}
+			})
+			
+			.state('app.elections', {
+				url: "/elections",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/elections.html"
+					}
+				}
+			})
+
+			.state('app.participants', {
+				url: "/participants",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/participants.html"
+					}
+				}
+			})
+
+			.state('app.files', {
+				url: "/files",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/files.html",
+					}
+				}
+			})
+			
+			.state('app.settings', {
+				url: "/settings",
+				views: {
+					'menuContent' : {
+						templateUrl: "templates/settings.html",
+					}
+				}
+			});
+			
+
+  		$urlRouterProvider.otherwise('/app/files');		
+	});
+		
