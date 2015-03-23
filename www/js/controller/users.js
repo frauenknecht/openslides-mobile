8e8c54ce4cbbb570314e290d66019c10d6f7d31e
Stefan Frauenknecht <stefan.frauenknecht@gmail.com>
2015-03-23 22:08:16 +0100

    Use collection-repeat instead of ng-repeat

--- www/js/controller/users.js
+++ www/js/controller/users.js
@@ -5,11 +5,43 @@
 	var osl = angular.module('osl')
 
 	osl.controller('UsersCtrl', UsersCtrl);		
-	UsersCtrl.$inject = [ 'Users' ];
+
+	UsersCtrl.$inject = [ '$filter', 
+						  'Users' ];
 		
-	function UsersCtrl (Users) {		
-		var vm = this;			
-		vm.users = Users.all();			
+	function UsersCtrl ($filter, Users) {		
+		var vm = this;
+		
+		vm.letters = [];			
+		vm.users = [];
+		
+		vm.getItemHeight = getItemHeight;
+		vm.getItemWidth = getItemWidth;
+		vm.scrollBottom = scrollBottom;
+		
+		init();
+		
+		
+		function init() {
+			vm.users = Users.all();
+		}
+		
+		function getItemHeight (item) {
+			return item.isLetter ? 40 : 100;
+		}	
+		
+		function getItemWidth () {
+			return '100%';
+		}	
+
+		function scrollTop () {
+			$ionicScrollDelegate.scrollTop(true);
+		}	
+		
+		function scrollBottom () {
+			$ionicScrollDelegate.scrollBottom(true);
+		}	
+		
 	};
 	
 	
