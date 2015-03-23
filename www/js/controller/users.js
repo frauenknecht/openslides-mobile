5d0593ac7a292097a2153a3a2da35d09108ad6d1
Stefan Frauenknecht <stefan.frauenknecht@gmail.com>
2015-03-23 18:34:35 +0100

    User service fixed.

--- www/js/controller/users.js
+++ www/js/controller/users.js
@@ -1,52 +1,9 @@
 angular.module('osl')
 
-.factory('Users', ['$http', function ($http) {
 
-	var users = [
-    	{
-         	id				: "1",
-         	is_present		: true,
-			title			: "",
-			first_name		: "Ashok",
-			last_name		: "Kumar",
-			structure_level	: "Indien",
-			about_me		: "",
-			comment			: "",
-			groups			: "Delegierte/r"
-    	},
-
-		{
-         	id				: "2",
-         	is_present		: false,
-			title			: "",
-			first_name		: "Bob",
-			last_name		: "Smith",
-			structure_level	: "Neuseeeland",
-			about_me		: "",
-			comment			: "",
-			groups			: ""
-		}
-	];
-
-  	return {
-	
-		all : function() {
-			return users;			
-		},
-	
-		find : function(id) {
-			for(i=0; i<users.length; i++){
-				if(users[i].id == id){
-					return users[i];
-				}	
-			}
-		}  		
-  	}
-
-}])
 
 .controller('UsersCtrl', function ($scope, $state, Users) {	
-	$scope.users = Users.all();	
+	$scope.users = users;
 })
 
 
